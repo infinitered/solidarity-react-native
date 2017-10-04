e4c2cd4f5eb03b1af577da10175e428561a526c3
Gant <GantMan@gmail.com>
2017-10-04 00:27:09 -0500

    super basic plugin stub

---
+++ extensions/react-native.js
@@ -0,0 +1,7 @@
+module.exports = (context) => {
+  // Register this plugin
+  context.pluginsList.push({
+    name: 'React Native',
+    description: 'Snapshot solidarity rules for React Native projects'
+  })
+}
