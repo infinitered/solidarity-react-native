717ff8cfccc6ebf0d1b3153463d0049fbb8194d0
Gant <GantMan@gmail.com>
2017-10-07 18:35:26 -0500

    functioning plugin

--- extensions/react-native.js
+++ extensions/react-native.js
@@ -2,6 +2,16 @@ module.exports = (context) => {
   // Register this plugin
   context.pluginsList.push({
     name: 'React Native',
-    description: 'Snapshot solidarity rules for React Native projects'
+    description: 'Snapshot solidarity rules for React Native projects',
+    snapshot: async (context) => {
+      const { filesystem, system } = context
+      // Copy template
+      filesystem.copy(
+        `${__dirname}/../templates/react-native-template.ejs`,
+        '.solidarity'
+      )
+      // Update versions to local
+      await system.run('solidarity snapshot')
+    }
   })
 }
