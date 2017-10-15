b55745982b007e0f8d78a37c57b850ca27834bcc
Gant <GantMan@gmail.com>
2017-10-15 00:09:51 -0500

    advanced react-native features

---
+++ extensions/helpers/addOptionalRules.js
@@ -0,0 +1,31 @@
+module.exports = (context, requirements) => {
+  const { solidarity, filesystem } = context
+  const { binaryExists } = solidarity
+  // Rules we are considering adding
+  const mobileCenter = [{rule: 'cli', binary: 'mobile-center', semver: '0.0.0'}]
+  const codePush = [{rule: 'cli', binary: 'code-push', semver: '0.0.0'}]
+  const npm = [{rule: 'cli', binary: 'npm', semver: '0.0.0'}]
+  const yarn = [{rule: 'cli', binary: 'yarn', version: '--version', semver: '0.0.0'}]
+  const pod = [{rule: 'cli', binary: 'pod', platform: 'darwin', semver: '0.0.0'}]
+
+  // Add if binary exists
+  if (binaryExists('mobile-center', context)) {
+    requirements['Mobile Center'] = mobileCenter
+  }
+  if (binaryExists('code-push', context)) {
+    requirements['Code Push'] = codePush
+  }
+  if (binaryExists('npm', context)) {
+    requirements['NPM'] = npm
+  }
+
+  // Add if binary AND yarn.lock exits
+  if (binaryExists('yarn', context) && filesystem.exists('./yarn.lock') === 'file') {
+    requirements['Yarn'] = yarn
+  }
+
+  // Add if binary AND ios/Podfile.lock exits
+  if (binaryExists('pod', context) && filesystem.exists('./ios/Podfile.lock') === 'file') {
+    requirements['Cocoapods'] = pod
+  }
+}
