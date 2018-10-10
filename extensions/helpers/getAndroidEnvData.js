b40d320be5c48e5ff54e02e3141e6cf83903cacf
Daniel Schmidt <dschmidt@mesosphere.com>
2018-10-10 23:15:30 +0200

    fix errors with android in newer react native versions
    Newer RN versions use the
    
    ```gradle
    android {
        compileSdkVersion rootProject.ext.compileSdkVersion
        buildToolsVersion rootProject.ext.buildToolsVersion
    }
    ```
    syntax which was not supported and lead to an immediate exit.

--- extensions/helpers/getAndroidEnvData.js
+++ extensions/helpers/getAndroidEnvData.js
@@ -1,18 +1,33 @@
-module.exports = (context) => {
-  const { envInfo, filesystem } = context
-  // Version specified by RN project settings
-  const androidGradle = filesystem.read('./android/app/build.gradle')
-  const androidData = envInfo.getAllAndroidSDKs()
-
-  if (androidGradle) {
-    return {
-      androidGradle,
-      availableApiVersions: androidData['API Levels'],
-      availableBuildToolsVersions: androidData['Build Tools'],
-      projectApiVersion: /compileSdkVersion\s(\d+)/.exec(androidGradle)[1],
-      projectBuildToolsVersion: /buildToolsVersion\s["|']([\d|.]+)["|']/.exec(androidGradle)[1]
-    }
-  } else {
-    return { androidGradle: null }
+const executeRegexOnFiles = (regex, files) => {
+  for (let file of files) {
+    try {
+      return regex.exec(file)[1];
+    } catch (e) {}
   }
-}
+};
+
+module.exports = context => {
+  const { envInfo, filesystem } = context;
+  // Version specified by RN project settings
+  const androidAppGradle = filesystem.read("./android/app/build.gradle");
+  const androidGradle = filesystem.read("./android/build.gradle");
+  const androidData = envInfo.getAllAndroidSDKs();
+
+  if (androidAppGradle) {
+    return {
+      androidAppGradle,
+      availableApiVersions: androidData["API Levels"],
+      availableBuildToolsVersions: androidData["Build Tools"],
+      projectApiVersion: executeRegexOnFiles(/compileSdkVersion\s=?\s?(\d+)/, [
+        androidAppGradle,
+        androidGradle
+      ]),
+      projectBuildToolsVersion: executeRegexOnFiles(
+        /buildToolsVersion\s=?\s?["|']([\d|.]+)["|']/,
+        [androidAppGradle, androidGradle]
+      )
+    };
+  } else {
+    return { androidAppGradle: null };
+  }
+};
